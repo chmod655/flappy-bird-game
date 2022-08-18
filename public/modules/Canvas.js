14a37e0a09dcf2b319c2960cc2c1bfbefb676876
chmod655 <andraderennan099@gmail.com>
2022-08-18 19:37:59 -0300

    arquivo apenas para conter o contexto e o canvas junto do arquivo de sprites

---
+++ public/modules/Canvas.js
@@ -0,0 +1,8 @@
+// Tag canvas
+export const canvas = document.querySelector('canvas')
+// Criando o context
+export const ctx = canvas.getContext('2d')
+// Carregando sprites
+export const sprites = new Image()
+sprites.src = 'assets/sprites/sprites.png'
+

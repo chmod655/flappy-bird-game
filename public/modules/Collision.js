68df1553f7f4ba4121b8bcc47ebe434350e3d353
chmod655 <andraderennan099@gmail.com>
2022-08-19 00:45:19 -0300

    Organização no arquivo Collision.js

--- public/modules/Collision.js
+++ public/modules/Collision.js
@@ -2,6 +2,9 @@ export function Collision(player, floor) {
     const playerY = player.y + player.cropY;
     const floorY = floor.y
 
-    if (playerY >= floorY) return true
+    if (playerY >= floorY)
+    {
+        return true
+    }
     return false
 }

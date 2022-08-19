22faba75eae6aebe3996eb80ce26c5055c174bfc
chmod655 <andraderennan099@gmail.com>
2022-08-19 01:26:34 -0300

    Main script && css fix

--- public/app.js
+++ public/app.js
@@ -1,13 +1,11 @@
-import { Screens, changeToScreen, screenActive } from './modules/Scenes.js'
-
-let frames = 0
+import { Screens, changeToScreen, screenActive, globals } from './modules/Scenes.js'
 
 function main() 
 {
     screenActive.draw()
     screenActive.update()
 
-    frames += 1
+    globals.frames += 1
     requestAnimationFrame(main)
 }
 

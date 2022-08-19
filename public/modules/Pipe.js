36de9a68071fe646d70b1db06c68ed0e86506b10
chmod655 <andraderennan099@gmail.com>
2022-08-19 00:47:51 -0300

    import do scenes por conta de colisão que precisa retornar para tela inicial Fixei algumas coisas como - spriteX, spriteY - tambem fixei a largura do pipe de cima - o spacingBettweenPipes teve o valor alterado de  (70) para (100) - organizei um pouco mais o drawImage que antes estava tudo na mesma linha
    Commit de mudanças no Pipe.js

--- public/modules/Pipe.js
+++ public/modules/Pipe.js
@@ -1,14 +1,15 @@
-import { canvas, ctx, sprites } from "./Canvas.js"
+import { canvas, ctx, sprites } from './Canvas.js'
+import { changeToScreen, globals, Screens } from './Scenes.js'
 
 export function inicializePipe() {
     const pipe = {
-        spriteX: 52, spriteY: 300, // Sprite Pos, X, Y
+        spriteX: 52, spriteY: 300, // Sprite Pos, X, Y width ,height
         floor: {
             spriteX: 0,
             spriteY: 169,
         },
         sky: {
-            spriteX: 54,
+            spriteX: 52,
             spriteY: 269,
         },
         spacing: 80,
@@ -16,17 +17,21 @@ export function inicializePipe() {
         draw() {
             this.pairs.forEach(pair => {
                 const randomY = pair.y
-                const spacingBettweenPipes = 70
+                const spacingBettweenPipes = 100
                 
                 // Pipe Sky
                 const skyX = pair.x
                 const skyY = randomY
                 ctx.drawImage(
                     sprites,
-                    this.sky.spriteX, this.sky.spriteY,
-                    this.spriteX, this.spriteY,
-                    skyX, skyY,
-                    this.spriteX, this.spriteY,
+                    this.sky.spriteX, 
+                    this.sky.spriteY,
+                    this.spriteX, 
+                    this.spriteY,
+                    skyX, 
+                    skyY,
+                    this.spriteX, 
+                    this.spriteY,
                 )
 
                 // Pipe Floor
@@ -39,18 +44,47 @@ export function inicializePipe() {
                     floorX, floorY,
                     this.spriteX, this.spriteY,
                 )
+
+                pair.sky = {
+                    x: skyX,
+                    y: this.spriteY + skyY
+                }
+                pair.floor = {
+                    x: floorX,
+                    y: floorY
+                }
             })
         },
+        hasCollisionPlayer(pair) {
+			const headPlayer = globals.Player.y
+			const footPlayer = globals.Player.y + globals.Player.cropY
+			if ((globals.Player.x + globals.Player.cropX) >= pair.x) {
+				if (headPlayer <= pair.sky.y) {
+                    console.log(pair.x)
+                    return true
+                }
+				if (footPlayer >= pair.floor.y) {
+                    console.log(pair.y)
+                    return true
+                }
+			}
+            return false
+		},
         update() {
-            const passedOneHundredFrames = frames % 100 === 0
+            const passedOneHundredFrames = globals.frames % 100 === 0
+
             if (passedOneHundredFrames) {
                 this.pairs.push({
                     x: canvas.width,
-                    y:  -200 * Math.random() + 2
+                    y:  -150 * (Math.random() + 1) // Worked
                 })
             }
             this.pairs.forEach(pair => {
                 pair.x = pair.x -2
+
+                if (this.hasCollisionPlayer(pair)) {
+                    changeToScreen(Screens.start)
+                }
 
                 if(pair <= 0) this.pairs.shift()
             })
